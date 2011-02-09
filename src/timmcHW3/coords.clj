6808b82aa75fa28137af7968d82878de6fae7d78
Tim McCormack <cortex@brainonfire.net>
2011-02-08 19:11:50 -0500

    Might actually have a working transform. Draws a little red square in the canvas center = world origin.

---
+++ src/timmcHW3/coords.clj
@@ -0,0 +1,89 @@
+(ns timmcHW3.coords
+   "Coordinates and transformations."
+   (:import [java.awt.geom Point2D$Double]))
+
+;-- Conventions --;
+
+;;; World locations and offsets are in homogeneous coordinates.
+;;; Viewport locations and offsets are Point2D
+;;; Matrices are all 3x3 and represented as a column vector of 3 row vectors.
+;;; All vectors are 3-element and are 1-level Clojure vectors.
+
+;-- Math --;
+
+(defn mat3xm
+   "Multiply zero or more 3x3 matrices."
+   ([] [[1 0 0][0 1 0][0 0 1]])
+   ([a] a)
+   ([a b]
+    (let [[[a00 a01 a02]
+           [a10 a11 a12]
+           [a20 a21 a22]] a
+          [[b00 b01 b02]
+           [b10 b11 b12]
+           [b20 b21 b22]] b]
+       [[(+ (* a00 b00) (* a01 b10) (* a02 b20))
+         (+ (* a00 b01) (* a01 b11) (* a02 b21))
+         (+ (* a00 b02) (* a01 b12) (* a02 b22))]
+        [(+ (* a10 b00) (* a11 b10) (* a12 b20))
+         (+ (* a10 b01) (* a11 b11) (* a12 b21))
+         (+ (* a10 b02) (* a11 b12) (* a12 b22))]
+        [(+ (* a20 b00) (* a21 b10) (* a22 b20))
+         (+ (* a20 b01) (* a21 b11) (* a22 b21))
+         (+ (* a20 b02) (* a21 b12) (* a22 b22))]]))
+   ([a b & more]
+    (apply mat3xm (mat3xm a b) more)))
+
+(defn mat3xv
+   "Multiply a 3x3 matrix by a column vector."
+   [a v]
+   (let [[[a00 a01 a02]
+          [a10 a11 a12]
+          [a20 a21 a22]] a
+         [v0
+          v1
+          v2] v]
+      [(+ (* a00 v0) (* a01 v1) (* a02 v2))
+       (+ (* a10 v0) (* a11 v1) (* a12 v2))
+       (+ (* a20 v0) (* a21 v1) (* a22 v2))]))
+
+;-- Construction --;
+
+(defn mk-loc
+   "Make a 2D point from x and y coordinates."
+   [x y]
+   [x y 1])
+
+(defn mk-offset
+   "Make a 2D offset from x/y vector."
+   [x y]
+   [x y 0])
+
+(defn crd-x
+   "x component of a coordinate"
+   [c]
+   (c 0))
+
+(defn crd-y
+   "y component of a coordinate"
+   [c]
+   (c 1))
+
+;-- Transformations --;
+
+;-- Conversion --;
+
+(defn ^Point2D$Double world-to-view
+   "Transform world coordinates to view coordinates, dropping loc/offset distinction."
+   [wc tmat]
+   (let [[vx vy _] (mat3xv tmat wc)]
+      (Point2D$Double. vx vy)))
+
+(defn view-to-world
+   "Transform view coordinates (location or offset) to world coordinates."
+   [^Point2D$Double vc, tmat, is-offset]
+   (let [x (. vc x)
+         y (. vc y)
+         p (if is-offset 0 1)]
+      (mat3xv tmat [x y p])))
+
--- src/timmcHW3/core.clj
+++ src/timmcHW3/core.clj
@@ -2,35 +2,46 @@
    "Core code. Use -main."
    (:import [javax.swing SwingUtilities UIManager
                          JFrame JComponent JPanel JMenu JMenuBar JMenuItem JButton]
-            [java.awt Graphics2D RenderingHints Dimension Color]
-            [java.awt.geom Path2D Path2D$Double Point2D Point2D$Double]
+            [java.awt FlowLayout Graphics2D RenderingHints Dimension Color]
+            [java.awt.geom Path2D Path2D$Double Point2D Point2D$Double Rectangle2D$Double]
             [java.awt.event ActionListener])
+   (:use (timmcHW3 coords))
    (:gen-class))
 
 ;-- State --;
 
-(def ^{:doc "Control polygon as Point2D list"} ;TODO better than cubic
-   control-polygon (ref [(Point2D$Double. 100 100)
-                         (Point2D$Double. 100 150)
-                         (Point2D$Double. 150 150)
-                         (Point2D$Double. 150 100)]))
+(def ^{:doc "Control polygon as coords list"} ;TODO better than cubic
+   control-polygon (ref [(mk-loc -50 0)
+                         (mk-loc 0 30)
+                         (mk-loc 0 -30)
+                         (mk-loc 5 0)]))
+
+(defn ^Point2D$Double basic-trans ;XXX
+   "Basic transformation"
+   [wx wy]
+   (let [rot [[0.707 -0.707 0][0.707 0.707 0][0 0 1]]
+         scale [[0.5 0 0][0 -0.5 0][0 0 1]]
+         trans [[0 0 300][0 0 300][0 0 1]]
+         tmat (mat3xm trans rot scale)
+         [vx vy _] (mat3xv tmat [wx wy 1])]
+      (println wx wy "->" vx vy)
+      (Point2D$Double. vx vy)))
 
 ;-- Math --;
 
+;TODO: add {set,nudge}-{rotation,zoom,translation-{x,y}}! functions
+;TODO: add functions to apply transforms to collections of coords
 (defn ^Path2D calc-path
    "Calculate a path based on the current control points."
    [] ;TODO accept polyline as arg?
    (let [points @control-polygon
          path (Path2D$Double.)]
       (when (= (count points) 4)
-         (let [[^Point2D$Double p0
-                ^Point2D$Double p1
-                ^Point2D$Double p2
-                ^Point2D$Double p3] points]
-            (. path moveTo (. p0 x) (. p0 y))
-            (. path curveTo (. p1 x) (. p1 y)
-                            (. p2 x) (. p2 y)
-                            (. p3 x) (. p3 y))))
+         (let [[p0 p1 p2 p3] points]
+            (. path moveTo (crd-x p0) (crd-y p0))
+            (. path curveTo (crd-x p1) (crd-y p1)
+                            (crd-x p2) (crd-y p2)
+                            (crd-x p3) (crd-y p3))))
       path))
 
 ;-- Rendering --;
@@ -38,9 +49,11 @@
 (defn render
    "Draw the world."
    [^Graphics2D g]
-   (. g setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
-   (. g setColor Color/RED)
-   (. g draw (calc-path)))
+   (let [center (basic-trans 0 0)]
+      (. g setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
+      (. g setColor Color/RED)
+      (. g fill (Rectangle2D$Double. (. center x) (. center y) 3 3))
+      (. g draw (calc-path))))
 
 ;-- Menu items --;
 
@@ -66,6 +79,7 @@
    canvas
    (doto (proxy [JComponent] []
             (paint [^Graphics2D g] (render g)))
+      (.setDoubleBuffered true)
       (.setPreferredSize (Dimension. 600 600))))
 
 ;-- Setup --;
@@ -77,6 +91,7 @@
    (let [frame (doto (JFrame.)
                   (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
                   (.setJMenuBar menu)
+                  (.setLayout (FlowLayout. FlowLayout/LEFT 0 0))
                   (.add controls)
                   (.add canvas)
                   (.pack))]
