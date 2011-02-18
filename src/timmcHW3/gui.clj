841bc1ef3e723c3b71b8aef411b07b638c6f72bb
Tim McCormack <cortex@brainonfire.net>
2011-02-18 16:11:45 -0500

    Add labels for spinners.

--- src/timmcHW3/gui.clj
+++ src/timmcHW3/gui.clj
@@ -6,34 +6,65 @@
    (:import
       [java.awt Dimension Graphics2D Component BorderLayout]
       [javax.swing BoxLayout BorderFactory
-         JFrame JComponent JPanel JMenu JMenuBar JMenuItem JCheckBoxMenuItem JButton JSpinner SpinnerNumberModel JSpinner$NumberEditor
+         JFrame JComponent JPanel JMenu JMenuBar JMenuItem JCheckBoxMenuItem
+         JButton JLabel JSpinner SpinnerNumberModel JSpinner$NumberEditor
          KeyStroke]
       [javax.swing.border Border EtchedBorder]))
 
-; We define these in functions so they are not created at compile-time.
+;-- Utilities --;
 
-(defn ^JMenuItem new-mi-undo
+(defn- ^Border make-controls-border
+   [title]
+   (-> (BorderFactory/createEtchedBorder EtchedBorder/LOWERED)
+       (BorderFactory/createTitledBorder title)
+       (BorderFactory/createCompoundBorder (BorderFactory/createEmptyBorder 4 4 4 4))))
+
+(defn- ^Component make-vertical-layout
+   "Lay out a series of components as a vertical stack with an expanding south. Not tail-recursive."
+   [^int vgap, ^Component c & more]
+   (let [cont (JPanel.)]
+      (.setLayout cont (BorderLayout. 0 vgap))
+      (.add cont c BorderLayout/PAGE_START)
+      (when (seq more)
+         (.add cont ^Component (apply make-vertical-layout vgap more) BorderLayout/CENTER))
+      cont))
+
+(defn- ^Component add-label
+   "Return a component with a label attached to the given component."
+   [^String text, ^JComponent child]
+   (doto (JPanel.)
+      (.setLayout (BorderLayout. 4 0))
+      (.add (JLabel. text) BorderLayout/LINE_START)
+      (.add child BorderLayout/LINE_END)))
+
+;-- Component creators --;
+
+; We define these in functions so they are not created at compile-time.
+; These methods each take a ref to a GUI record that they can add their components to.
+
+(defn- ^JMenuItem new-mi-undo
    [rgui]
    (doto (JMenuItem. "Undo")
       (.setEnabled false)
       (.setAccelerator (KeyStroke/getKeyStroke "ctrl Z"))))
 
-(defn ^JMenuItem new-mi-redo
+(defn- ^JMenuItem new-mi-redo
    [rgui]
    (doto (JMenuItem. "Redo")
       (.setEnabled false)
       (.setAccelerator (KeyStroke/getKeyStroke "ctrl Y"))))
 
-(defn ^JMenuItem new-mi-exit
+(defn- ^JMenuItem new-mi-exit
    [rgui]
    (doto (JMenuItem. "Exit")
       (.setAccelerator (KeyStroke/getKeyStroke "ctrl Q"))))
 
-(defn ^JCheckBoxMenuItem new-mi-view-control
+(defn- ^JCheckBoxMenuItem new-mi-view-control
    [rgui]
-   (JCheckBoxMenuItem. "Show control polygon" true))
+   (doto (JCheckBoxMenuItem. "Show control polygon" true)
+      (.setAccelerator (KeyStroke/getKeyStroke "ctrl P"))))
 
-(defn ^JMenuBar new-menu
+(defn- ^JMenuBar new-menu
    "Make a menu bar."
    [rgui]
    (doto (JMenuBar.)
@@ -44,23 +75,7 @@
       (.add (doto (JMenu. "View")
                (.add (create! rgui [:mi-view-control] new-mi-view-control rgui))))))
 
-(defn ^Border make-controls-border
-   [title]
-   (-> (BorderFactory/createEtchedBorder EtchedBorder/LOWERED)
-       (BorderFactory/createTitledBorder title)
-       (BorderFactory/createCompoundBorder (BorderFactory/createEmptyBorder 4 4 4 4))))
-
-(defn ^Component make-vertical-layout
-   "Lay out a series of components as a vertical stack with an expanding south. Not tail-recursive."
-   [c & more]
-   (let [cont (JPanel.)]
-      (.setLayout cont (BorderLayout.))
-      (.add cont c BorderLayout/NORTH)
-      (when (seq more)
-         (.add cont (apply make-vertical-layout more) BorderLayout/CENTER))
-      cont))
-
-(defn ^JSpinner new-pose-rotate
+(defn- ^JSpinner new-pose-rotate
    "Make the spinner for viewpoint rotation."
    [rgui]
    (let [nm (SpinnerNumberModel. 0.0 nil nil 0.03) ; radians!
@@ -70,7 +85,7 @@
       (.setEditor js ned)
       js))
 
-(defn ^JSpinner new-pose-zoom
+(defn- ^JSpinner new-pose-zoom
    "Make the spinner for viewpoint zooming."
    [rgui]
    (let [nm (SpinnerNumberModel. 1.0 0.001 20.0 0.003) ; log scale, higher is greater mag
@@ -80,24 +95,26 @@
       (.setEditor js ned)
       js))
 
-(defn ^JPanel new-pose-panel
+(defn- ^JPanel new-pose-panel
    "Make a Pose panel."
    [rgui]
-   (let [p (JPanel.)]
-      (doto p
-         (.setBorder (make-controls-border "Position"))
-         (.add (create! rgui [:spinner-rot] new-pose-rotate rgui))
-         (.add (create! rgui [:spinner-zoom] new-pose-zoom rgui)))))
+   (doto (JPanel.)
+      (.setBorder (make-controls-border "Position"))
+      (.add (make-vertical-layout 4
+               (add-label "Rotation:"
+                  (create! rgui [:spinner-rot] new-pose-rotate rgui))
+               (add-label "Zoom:"
+                  (create! rgui [:spinner-zoom] new-pose-zoom rgui))))))
 
-(defn ^JPanel new-controls
+(defn- ^JPanel new-controls
    "Make a control panel."
    [rgui]
    (let [p (JPanel.)]
       (doto p
-         (.add (make-vertical-layout
+         (.add (make-vertical-layout 0
                   (new-pose-panel rgui))))))
 
-(defn ^JComponent new-canvas
+(defn- ^JComponent new-canvas
    "Make a drawing canvas."
    [rgui renderer]
    (let [jc (proxy [JComponent] []
