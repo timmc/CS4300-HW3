15d974b8b2ea4e36c195540b46600b63941b80c6
Tim McCormack <cortex@brainonfire.net>
2011-02-20 13:31:15 -0500

    Add test for picking.

---
+++ test/timmcHW3/test/core.clj
@@ -0,0 +1,13 @@
+(ns timmcHW3.test.core
+   (:use [timmcHW3.core] :reload)
+   (:use [timmcHW3.state] :reload)
+   (:import [timmcHW3.state UserData])
+   (:use [clojure.test])
+   (:import [java.awt.geom Point2D Point2D$Double]))
+
+(deftest picking
+   (let [target (Point2D$Double. 100 100)]
+      (is (pick-vertex? (- 100 pick-radius) 100 target))
+      (is (pick-vertex? (+ 100 pick-radius) 100 target))
+      (is (not (pick-vertex? (- 100 pick-radius 1) 100 target)))
+      (is (not (pick-vertex? (+ 100 pick-radius 1) 100 target)))))
