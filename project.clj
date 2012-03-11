2c05d5a5514bf5b859c63f31734d7e3d0552ec97
Tim McCormack <cortex@brainonfire.net>
2012-03-10 22:30:05 -0500

    No need to rely on contrib

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject timmcHW3 "1.1.1"
   :description "Tim McCormack HW3 submission for CS 4300, Spring 2011"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.timmc/cascade "0.1.0"]]
   :main timmcHW3.core
   ;:warn-on-reflection true
